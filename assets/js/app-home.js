3f3ab5377d42c26bd5e56c4c4238f5762ef92269
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-07-03 13:19:00 -0400

    se descargan las imagenes necesarias

--- assets/js/app-home.js
+++ assets/js/app-home.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 	//agregamos logo
 	$("#cont-logo").append('<img id="logo"></img>');
 	$("#logo").attr("src","https://d24wuq6o951i2g.cloudfront.net/img/events/id/247/2471884/assets/fc0.logo_standard_silver.png").addClass("img-responsive");
-	
+
 	//llamamos a la ID del botón Sign Up
 	$("#btn-signUp").click(function(){
 		$(this).attr("href","index-signup-country.html");
