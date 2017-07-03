00877018707c813ac0a51bab5a1a461e8a164e1b
karina palacios <karina.spalacios@gmail.com>
2017-07-03 13:12:18 -0400

    banderitas listas

--- assets/js/main-signup.js
+++ assets/js/main-signup.js
@@ -1,15 +1,4 @@
-var count = 0;
-
-$(".digit").on('click', function() {
-  var num = ($(this).clone().children().remove().end().text());
-  if (count < 11) {
-    $("#output").append('<span>' + num.trim() + '</span>');
-
-    count++
-  }
-});
-
-$('.fa-long-arrow-left').on('click', function() {
-  $('#output span:last-child').remove();
-  count--;
-});
+  $(document).ready(function() {
+    $('select').material_select();
+  });
+ $('select').material_select('destroy');
