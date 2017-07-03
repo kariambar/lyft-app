514fbecc43a49ae4416141ba56b1b12a24f5fe8f
ingriarce4 <ingridarce4@gmail.com>
2017-07-03 15:13:03 -0400

    cambios js y html

--- assets/js/app_mapa.js
+++ assets/js/app_mapa.js
@@ -1,68 +1,22 @@
-function initMap(){
-  var map = new google.maps.Map(document.getElementById("mapa"),{
-    zoom: 7,
-    center: {lat: -33.4724712, lng: -70.9107133},
-    mapTypeControl: false,
-    zoomControl: false,
-    streetViewControl: false
-  });
 
-
-  function buscar(){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
-    }
-  }
-
-  var latitud, longitud;
-
-  var funcionExito = function(posicion){
-    latitud = posicion.coords.latitude;
-    longitud = posicion.coords.longitude;
-
-    var miUbicacion = new google.maps.Marker({
-      position : {lat: latitud, lng: longitud},
-      animation: google.maps.Animation.DROP,
-      map: map,
-    });
-
-    map.setZoom(17);
-    map.setCenter({lat: latitud, lng: longitud});
-  }
-
-  var funcionError = function(error){
-    alert("Tenemos problemas encontrando tu ubicación");
-  }  
-
-  buscar();
-
-//autocompletado y luego declaro las dos variables que son funciones de google.
-  var start = document.getElementById("start");
-  var autocomplete = new google.maps.places.Autocomplete(start);
-  autocomplete.bindTo('bounds', map);
-
-  var end = document.getElementById("end");
-  var autocomplete = new google.maps.places.Autocomplete(end);
-  autocomplete.bindTo('bounds', map);
-
-
-  var directionsDisplay = new google.maps.DirectionsRenderer();
-  var directionsService = new google.maps.DirectionsService();
-
-   
-       /*ejemplo extraído de google donde me cambian dos direcciones*/
-
-  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    directionsService.route({
-      origin: document.getElementById('start').value,
-      destination: document.getElementById('end').value,
-      travelMode: 'DRIVING'
-    }, function(response, status) {
-      if (status === 'OK') {
-        directionsDisplay.setDirections(response);
-      } else {
-        window.alert('Fallo debido a ' + status);
-      }
-    });
-  }
-    }
+function tarifa() {
+       var directionsService = new google.maps.DirectionsService;
+       var directionsDisplay = new google.maps.DirectionsRenderer;
+       var map = new google.maps.Map(document.getElementById('map'), {
+         zoom: 18,
+         center: {lat: -34.397, lng: 150.644},
+       });
+       directionsDisplay.setMap(map);        var input2 = (document.getElementById("destino"));
+       var autocomplete = new google.maps.places.Autocomplete(input2);
+         autocomplete.bindTo("bounds", map);
+       var infoWindow = new google.maps.InfoWindow({map: map});        if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(function(position) {
+           var location = {
+             lat: position.coords.latitude,
+             lng: position.coords.longitude
+     };
+         infoWindow.setPosition(location);
+         infoWindow.setContent("Estas aquí");
+         map.setCenter(location);
+       },
+  
